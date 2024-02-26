8a1300965a7ce32f2478d76118e902cebf6df50d
Dev Patel <dev2104patel@gmail.com>
2024-02-26 13:20:21 -0400

    setting up basic folder structure

---
+++ app/src/main/java/com/mobile/healthsync/PatientDashboard.kt
@@ -0,0 +1,11 @@
+package com.mobile.healthsync
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_dashboard)
+    }
+}
