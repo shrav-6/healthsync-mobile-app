3e48fde8df52237e5cfac961ae99a84f1a0e8cec
rabiaa <rb618118@dal,ca>
2024-02-27 12:22:21 -0400

    feat: setting up backend

--- app/src/main/java/com/mobile/healthsync/views/patientProfile/PatientProfile.kt
+++ app/src/main/java/com/mobile/healthsync/views/patientProfile/PatientProfile.kt
@@ -1,6 +1,8 @@
 package com.mobile.healthsync.views.patientProfile
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 import com.mobile.healthsync.R
@@ -11,6 +13,16 @@ class PatientProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_profile)
 
+        // Initialize views
+        val firstNameEditText:EditText = findViewById(R.id.firstNameEditText)
+        val lastNameEditText:EditText = findViewById(R.id.lastNameEditText)
+        val weightEditText:EditText = findViewById(R.id.weightEditText)
+        val heightNameEditText:EditText = findViewById(R.id.heightEditText)
+
+        // Initialize other views
+
+        // Fetch patient data from Firebase
+        fetchPatientData()
     }
 
     private fun fetchPatientData() {
