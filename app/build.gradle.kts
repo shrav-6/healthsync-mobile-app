adc59cb8b5518444ec23950f9cf7da48a704b43c
Dev Patel <dev2104patel@gmail.com>
2024-02-27 00:49:50 -0400

    Added patient can see doctor list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -61,8 +61,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-firestore")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/mobile/healthsync/views/patientDashboard/PatientDashboard.kt
+++ app/src/main/java/com/mobile/healthsync/views/patientDashboard/PatientDashboard.kt
@@ -1,12 +1,41 @@
 package com.mobile.healthsync.views.patientDashboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mobile.healthsync.R
+import com.mobile.healthsync.adapters.DoctorAdapter
+import com.mobile.healthsync.model.Doctor
+import com.mobile.healthsync.repository.DoctorRepository
 
 class PatientDashboard : AppCompatActivity() {
+
+    private  lateinit var doctorAdapter: DoctorAdapter
+    private  lateinit var doctorRepository: DoctorRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_dashboard)
+
+        doctorRepository = DoctorRepository(this)
+        var doctorsList : MutableList<Doctor> = mutableListOf()
+
+        doctorRepository.getAllDoctors { retrievedDoctorsList ->
+            doctorsList = retrievedDoctorsList
+
+            var rvDoctorList = findViewById<RecyclerView>(R.id.rvDoctorsList)
+
+            doctorAdapter = DoctorAdapter(doctorsList);
+
+            rvDoctorList.adapter = doctorAdapter
+            rvDoctorList.layoutManager = LinearLayoutManager(this)
+
+            Log.d("DoctorData", "Number of Doctors: ${doctorsList.size}")
+        }
+
+
     }
+
+
 }
