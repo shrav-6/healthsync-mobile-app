11c182e238f873f8eda192189ebb622104f5b143
Vishaka Vinod <vs235403@dal.ca>
2024-03-01 17:16:47 -0400

    feat: Finalized doctor profile UI

--- app/src/main/java/com/mobile/healthsync/views/doctorProfile/DoctorProfile.kt
+++ app/src/main/java/com/mobile/healthsync/views/doctorProfile/DoctorProfile.kt
@@ -13,20 +13,6 @@ class DoctorProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_profile)
 
-        // Your data source
-        val dataList = ArrayList<String>()
-        dataList.add("myItem1")
-        dataList.add("myItem2")
 
-        // Your adapter
-        val adapter = ArrayAdapter(this, R.layout.activity_doctor_profile, dataList)
-
-        // Your GridView
-        val gridView = findViewById<GridView>(R.id.doctorInfo)
-        gridView.adapter = adapter
-
-        // Add or remove items as needed and notify the adapter
-        dataList.add("Age")
-        adapter.notifyDataSetChanged()
     }
 }
