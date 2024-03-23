9d3ad592b908144df141cd383548fff10ec516ba
sshaji <sr569206@dal.ca>
2024-03-23 13:53:56 -0300

    feat: bringing back main

--- app/src/main/java/com/mobile/healthsync/MainActivity.kt
+++ app/src/main/java/com/mobile/healthsync/MainActivity.kt
@@ -1,42 +1,23 @@
-//package com.mobile.healthsync
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.os.Handler
-//import androidx.activity.ComponentActivity
-//import com.mobile.healthsync.views.signUp.SignupActivity
-//
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        val handler = Handler()
-//        handler.postDelayed({
-//            val intent = Intent(
-//                this@MainActivity,
-//                SignupActivity::class.java
-//            )
-//            startActivity(intent)
-//        }, 3000)
-//    }
-//}
 package com.mobile.healthsync
+
 import android.content.Intent
 import android.os.Bundle
-import android.os.Parcelable
+import android.os.Handler
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.res.integerArrayResource
-import com.mobile.healthsync.model.Doctor
-import com.mobile.healthsync.views.patientDashboard.DoctorInfoActivity
-import com.mobile.healthsync.views.patientDashboard.PatientDashboard
+import com.mobile.healthsync.views.signUp.SignupActivity
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, DoctorInfoActivity::class.java)
-        intent.putExtra("doctor_id",663)
-        intent.putExtra("patient_id",251)
-        startActivity(intent)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(
+                this@MainActivity,
+                SignupActivity::class.java
+            )
+            startActivity(intent)
+        }, 3000)
     }
 }
