b11d005c5321af0077f23962b06ac86ba016d7b3
Merin Mary Saju <mr457277@dal.ca>
2024-03-26 01:46:15 -0300

    "new changes"

--- app/src/main/java/com/mobile/healthsync/MainActivity.kt
+++ app/src/main/java/com/mobile/healthsync/MainActivity.kt
@@ -3,17 +3,32 @@ package com.mobile.healthsync
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.activity.ComponentActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
 import com.mobile.healthsync.services.AlarmScheduler
 import com.mobile.healthsync.services.AlarmScheduler2
-import com.mobile.healthsync.services.MyAlarmReceiver
-
 import com.mobile.healthsync.views.signUp.SignupActivity
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+// Get the Firebase token
+        // Get the Firebase token
+        /*FirebaseMessaging.getInstance().getToken()
+            .addOnCompleteListener { task: Task<String> ->
+                if (task.isSuccessful) {
+                    val token = task.result
+                    Log.d(TAG, "Firebase Token: $token")
+                    // Handle the token (e.g., send it to your server)
+                } else {
+                    Log.e(TAG, "Failed to get Firebase token: " + task.exception)
+                }
+            }*/
 
         val handler = Handler()
         handler.postDelayed({
