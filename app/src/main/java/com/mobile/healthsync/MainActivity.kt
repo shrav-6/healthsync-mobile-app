afe0834b86b3b76df54638f54ac32bc78f5778fe
Vishaka Vinod <vs235403@dal.ca>
2024-03-28 01:46:02 -0300

    reset main activity

--- app/src/main/java/com/mobile/healthsync/MainActivity.kt
+++ app/src/main/java/com/mobile/healthsync/MainActivity.kt
@@ -2,25 +2,24 @@ package com.mobile.healthsync
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
-import com.mobile.healthsync.views.doctorProfile.DoctorProfile
+
+import com.mobile.healthsync.views.signUp.SignupActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctor_profile)
+        setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, DoctorProfile::class.java)
-        startActivity(intent)
-
-//        val handler = Handler()
-//        handler.postDelayed({
-//            val intent = Intent(
-//                this@MainActivity,
-//                SignupActivity::class.java
-//            )
-//            startActivity(intent)
-//        }, 3000)
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(
+                this@MainActivity,
+                SignupActivity::class.java
+            )
+            startActivity(intent)
+        }, 3000)
 
     }
 }
