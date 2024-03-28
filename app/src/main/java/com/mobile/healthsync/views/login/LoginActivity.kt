d2f0e942e51f2d830f5531befd3066f59b7c883e
Pooja Chauhan <pj425390@dal.ca>
2024-03-28 16:16:11 -0300

    Login UI and XML changes

--- app/src/main/java/com/mobile/healthsync/views/login/LoginActivity.kt
+++ app/src/main/java/com/mobile/healthsync/views/login/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import com.mobile.healthsync.views.patientDashboard.PatientDashboard
 import com.mobile.healthsync.views.signUp.SignupActivity
@@ -64,19 +65,23 @@ class LoginActivity : AppCompatActivity() {
                         UserType.DOCTOR -> {
                             generateAndSaveToken(email, userType)
                             // Start Doctor Dashboard
+                            Toast.makeText(this, "Doctor login successful", Toast.LENGTH_SHORT).show()
                         }
 
                         UserType.PATIENT -> {
                             generateAndSaveToken(email, userType)
                             startActivity(Intent(this, PatientDashboard::class.java))
+                            Toast.makeText(this, "Patient login successful", Toast.LENGTH_SHORT).show()
                         }
                     }
                 } else {
                     Log.d("UserLogin", "User not found")
+                    Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
                 }
             }
             .addOnFailureListener { e ->
                 Log.e("UserLogin", "Error searching user: $e")
+                Toast.makeText(this, "Error occurred. Please try again later.", Toast.LENGTH_SHORT).show()
             }
     }
 
@@ -95,23 +100,54 @@ class LoginActivity : AppCompatActivity() {
 
     private fun saveTokenToFirebase(email: String, token: String, userType: UserType) {
         val db = FirebaseFirestore.getInstance()
-        val collection = when (userType) {
+        val accessTokensCollection = db.collection("accesstokens")
+
+        val userCollection = when (userType) {
             UserType.DOCTOR -> db.collection("doctors")
             UserType.PATIENT -> db.collection("patients")
         }
 
-        // Assuming you have a field named "token" in your FirebaseFirestore documents
-        collection.document(email).update("token", token)
-            .addOnSuccessListener {
-                Log.d(
-                    "LoginActivity",
-                    "Token added to ${userType.name.lowercase()} instance successfully"
-                )
+        Log.d("LoginActivity", "Fetching user document for email: $email")
+        // Get the user document based on email
+        userCollection.whereEqualTo("email", email)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    val userDocument = documents.documents[0]
+                    val userId = userDocument.id
+                    Log.d("LoginActivity", "User document found for email: $email")
+                    // Create a map with userId, userType, and token
+                    val accessTokenData = hashMapOf(
+                        "userId" to userId,
+                        "userType" to userType.name,
+                        "tokenId" to token
+                    )
+                    // Add the access token document to the accesstokens collection
+                    Log.d("LoginActivity", "Adding access token document for email: $email")
+                    accessTokensCollection.document(email).set(accessTokenData)
+                        .addOnSuccessListener {
+                            Log.d(
+                                "LoginActivity",
+                                "Token added to ${userType.name.lowercase()} instance successfully"
+                            )
+                        }
+                        .addOnFailureListener { e ->
+                            Log.e(
+                                "LoginActivity",
+                                "Error adding token to ${userType.name.lowercase()} instance: $e"
+                            )
+                        }
+                } else {
+                    Log.e(
+                        "LoginActivity",
+                        "User document not found for email: $email and userType: ${userType.name}"
+                    )
+                }
             }
             .addOnFailureListener { e ->
                 Log.e(
                     "LoginActivity",
-                    "Error adding token to ${userType.name.lowercase()} instance: $e"
+                    "Error retrieving user document: $e"
                 )
             }
     }
