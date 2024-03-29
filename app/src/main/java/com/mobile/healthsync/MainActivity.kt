cee616431095e26916402483f7f05d5e4e75531c
rabiaa <rb618118@dal,ca>
2024-03-28 22:44:55 -0300

    feat: attempting testing

--- app/src/main/java/com/mobile/healthsync/MainActivity.kt
+++ app/src/main/java/com/mobile/healthsync/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.ComponentActivity
-import com.mobile.healthsync.views.patientProfile.PatientProfile
+import com.mobile.healthsync.views.signUp.SignupActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         handler.postDelayed({
             val intent = Intent(
                 this@MainActivity,
-                PatientProfile::class.java
+                SignupActivity::class.java
             )
             startActivity(intent)
         }, 3000)
