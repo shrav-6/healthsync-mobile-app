e7a7c8b50648ca6fae91ecbb66b3616f990cfe4f
rabiaa <rb618118@dal,ca>
2024-03-31 01:50:46 -0300

    rewriting mainactivity

--- app/src/main/java/com/mobile/healthsync/MainActivity.kt
+++ app/src/main/java/com/mobile/healthsync/MainActivity.kt
@@ -4,24 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.ComponentActivity
-import com.mobile.healthsync.views.doctorProfile.DoctorProfile
-import com.mobile.healthsync.views.patientDashboard.PatientDashboard
+import com.mobile.healthsync.views.login.LoginActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val from = intent.getStringExtra("from")
-        if(from == "patient to do") {
-            val intent = Intent(this, PatientDashboard::class.java)
-            startActivity(intent)
-        }
+//        val from = intent.getStringExtra("from")
+//        if(from == "patient to do") {
+//            val intent = Intent(this, PatientDashboard::class.java)
+//            startActivity(intent)
+//        }
         val handler = Handler()
         handler.postDelayed({
             val intent = Intent(
                 this@MainActivity,
-                DoctorProfile::class.java
+                LoginActivity::class.java
             )
             startActivity(intent)
         }, 3000)
